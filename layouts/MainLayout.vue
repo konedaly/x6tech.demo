65c0b3459906d36790921f08013f7440c55ee03c
konedaly <konedaly@gmail.com>
2024-08-23 18:47:37 +0700

    add shoping button

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -57,18 +57,14 @@
 
                                 </div>
                             </div>
-                            <div class="border-b"/>
+                            <div class="border-b" />
                             <ul class="bg-[#faba00]">
                                 <li @click="navigateTo('/orders')"
-                                class="text=[13px] text-white py-2 px-4 w-full hover:bg-yellow-600 "
-                                >
-                                My Order
+                                    class="text=[13px] text-white py-2 px-4 w-full hover:bg-yellow-600 ">
+                                    My Order
                                 </li>
-                                <li 
-                                v-if="true"
-                                class="text=[13px] text-white py-2 px-4 w-full hover:bg-yellow-600 "
-                                >
-                                Sign out
+                                <li v-if="true" class="text=[13px] text-white py-2 px-4 w-full hover:bg-yellow-600 ">
+                                    Sign out
                                 </li>
 
                             </ul>
@@ -77,18 +73,76 @@
                 </li>
             </ul>
         </div>
-        <div
-        id="MainHeader"
-        class="flex items-center w-full bg-white"
-        >
-            <div class="flex lg:justify-start justify-between gap-10 max-w[1150px] w-full px-3 py-5 mx-auto" >
-                <NuxtLink to="/" class="min-w-[170px]" >
-                    <img 
-                    width="170"
-                    src="/public/x6tech.png" >
+        <div id="MainHeader" class="flex items-center w-full bg-white">
+            <div class="flex lg:justify-start justify-between gap-10 max-w[1150px] w-full px-3 py-5 mx-auto">
+                <NuxtLink to="/" class="min-w-[170px]">
+                    <img width="170" src="/public/x6tech.png">
 
                 </NuxtLink>
+                <div class="max-w-[700px] w-full md:block hidden">
+                    <div class="relative">
+                        <div class="flex items-center border-2 border-[#faba00] rounded w-full">
+                            <input class="
+                            w-full
+                            placeholder-gray-500
+                            text-sm
+                            pl-3
+                            focus:outline-none
+                            " placeholder="Search" type="text" v-model="SearchItem">
+                            <Icon v-if="isSearching" name="eos-icons:loading" size="25" class="mr-2" />
+                            <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#faba00]">
+                                <Icon name="ph:magnifying-glass" size="20" color="#fafafa" />
 
+                            </button>
+
+                        </div>
+                        <div class="absolute bg-white max-w-[700px] h-auto w-full">
+                            <div class="p-1">
+                                <NuxtLink to=" '/items/1'"
+                                    class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100">
+                                    <div class="flex items-center ">
+                                        <img class="rounded-md" width="40" src="http://picsum.photos/id/82/300/320">
+                                        <div class="truncate ml-2"> Test </div>
+
+                                    </div>
+                                    <div class="truncate"> $10</div>
+                                </NuxtLink>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <NuxtLink to="/shoppingcart" class="flex items-center">
+                    <button class="relative md:block hidden" @mouseenter="isCartHover = true"
+                        @mouseleave="isCartHover = false">
+                        <span class="
+                        absolute
+                        flex
+                        items-center
+                        justify-center
+                        -right-[3px]
+                        top-0
+                        bg-[#ff4646]
+                        h-[17px]
+                        min-w[17px]
+                        text-xs
+                        text-white
+                        px-0.5
+                        rounded-full
+                        ">
+                            0
+                        </span>
+                        <div class="min-w-[40px]">
+                            <Icon name="ph:shopping-cart-simple-light" size="33" :color="isCartHover ? '#ff4646' : ''" />
+
+
+                        </div>
+
+
+                    </button>
+                </NuxtLink>
+                
             </div>
         </div>
     </div>
@@ -96,6 +150,9 @@
 
 <script setup>
 let isAccountMenu = ref(false)
+let isSearching = ref(true)
+let SearchItem = ref('')
+let isCartHover = ref(false)
 
 
 </script>
