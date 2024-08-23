e5cc9bcff37f05f81a01a3ad2d129cfe9fc7c6e1
konedaly <konedaly@gmail.com>
2024-08-23 17:36:56 +0700

    add header menu

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -35,7 +35,7 @@
                     <Icon name="ic:sharp-install-mobile" size="17" />
                     App
                 </li>
-                <!..ktop meun>
+
                 <li @mouseenter="isAccountMenu = true" @mouseleave="isAccountMenu = false"
                     class="relative flex items-center px-2.5 hover:text-[#ff4646] h-full cursor-pointer"
                     :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgb(0,0,0,0.3)]' : 'border border-[#faba00]'">
@@ -48,21 +48,48 @@
                             <div class="text-semibold text-center text-[15px] w-full  leading-7 "> Welcome to X6tech!
                                 <div class="flex items-center gap-1 px-3 mb-3">
 
-                                    <NuxtLink to="/auth" class="bg-[#faba00] text-center w-full text=[15px] rounded text-white font-semibold  ">
+                                    <NuxtLink to="/auth"
+                                        class="bg-[#faba00] text-center w-full text=[15px] rounded text-white font-semibold  ">
+
                                         Login / Register
                                     </Nuxtlink>
 
 
                                 </div>
-
                             </div>
+                            <div class="border-b"/>
+                            <ul class="bg-[#faba00]">
+                                <li @click="navigateTo('/orders')"
+                                class="text=[13px] text-white py-2 px-4 w-full hover:bg-yellow-600 "
+                                >
+                                My Order
+                                </li>
+                                <li 
+                                v-if="true"
+                                class="text=[13px] text-white py-2 px-4 w-full hover:bg-yellow-600 "
+                                >
+                                Sign out
+                                </li>
 
+                            </ul>
                         </div>
                     </div>
-
-
                 </li>
             </ul>
+        </div>
+        <div
+        id="MainHeader"
+        class="flex items-center w-full bg-white"
+        >
+            <div class="flex lg:justify-start justify-between gap-10 max-w[1150px] w-full px-3 py-5 mx-auto" >
+                <NuxtLink to="/" class="min-w-[170px]" >
+                    <img 
+                    width="170"
+                    src="/public/x6tech.png" >
+
+                </NuxtLink>
+
+            </div>
         </div>
     </div>
 </template>
